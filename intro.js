08fceb351f4ce9e7c9174f796aa50e7a17c9b7ce
Afolabi Tobiloba <tobilobaafolabi24@gmail.com>
2026-06-12 18:58:07 +0100

    Initial commit: Assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+const user ={
+    name: "Afolabi Tobiloba",
+    age: 21,
+    favoriteprogramminglanguage: "Python,"
+}
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.favoriteprogramminglanguage)
